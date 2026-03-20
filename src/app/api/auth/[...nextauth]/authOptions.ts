7d84cec5be1aedfa12acfdba4bb45518dabdbcbf
PHTTT-O <phetphacahra123@gmail.com>
2026-03-20 21:17:25 +0700

    fix 2

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,62 @@
+import {AuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import  userLogIn  from "@/libs/userLogIn";
+
+export const  authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text",  placeholder: "email"},
+        password: { label: "Password", type: "password", placeholder: "password" },
+      },
+
+      async authorize(credentials) {
+        try {
+          const data:any = await userLogIn(
+            credentials!.email,
+            credentials!.password
+          );
+
+          return {
+            id: data._id,
+            name: data.name,
+            email: data.email,
+            role: data.role,
+            token: data.token,
+          };
+        } catch {
+          return null;
+        }
+      },
+    }),
+  ],
+
+  session: { strategy: "jwt" },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const u = user as any;
+        token._id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = u.role;
+        token.accessToken = u.token;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.user = {
+        _id: token._id as string,
+        name: token.name as string,
+        email: token.email as string,
+        role: token.role as string,
+        token: token.accessToken as string,
+      };
+
+      return session;
+    },
+  },
+};
